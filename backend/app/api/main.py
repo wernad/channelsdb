16835a3997fd2b759f6073382c42541ab4a2911b
chiro <jztnem@gmail.com>
2024-10-14 01:47:17 +0200

    add main router

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,5 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from endpoints import annotations, assembly, channels, content, download, statistics
 
+router = APIRouter()
+router.include_router(annotations.router, prefix="/annotations")
+router.include_router(assembly.router, prefix="/assembly")
+router.include_router(channels.router, prefix="/channels")
+router.include_router(content.router, prefix="/content")
+router.include_router(download.router, prefix="/download")
+router.include_router(statistics.router, prefix="/statistics")
 
 app = FastAPI(
     title="ChannelsDB 2.1 API",
