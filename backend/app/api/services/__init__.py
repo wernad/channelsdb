29a6f6993f5b8e46b8d884b964b7654bc7be16b7
chiro <jztnem@gmail.com>
2024-11-10 20:01:46 +0100

    move services out of api

--- backend/app/api/services/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.api.services.base import ServiceBase
-from app.api.services.channels import ChannelService
-from app.api.services.annotations import AnnotationService
--- backend/app/api/services/base.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import Session
-from app.database.repositories.base import RepositoryBase
-
-
-class ServiceBase:
-    _repository_class: RepositoryBase
-
-    def __init__(self, db: Session):
-        self.repository = self._repository_class(db)
