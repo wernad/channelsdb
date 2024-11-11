5d26d8cde641e8088d5fc230b7e7c38bf9932163
chiro <jztnem@gmail.com>
2024-11-12 00:56:40 +0100

    move export methods to its own service

--- backend/app/api/export/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.api.export.pdb import get_PDB_file
-from app.api.export.pymol import get_Pymol_file
-from app.api.export.chimera import get_Chimera_file
-from app.api.export.vmd import get_VMD_file
-from app.api.export.cif import get_cif
