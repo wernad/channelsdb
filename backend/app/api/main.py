9acb3596b4a9dafe19e6d242a9c5ee6e365aff94
chiro <jztnem@gmail.com>
2024-11-11 01:48:32 +0100

    formatting

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -9,9 +9,7 @@ from app.api.endpoints import (
 )
 
 api_router = APIRouter()
-api_router.include_router(
-    annotations.router, tags=["annotations"], prefix="/annotations"
-)
+api_router.include_router(annotations.router, tags=["annotations"], prefix="/annotations")
 api_router.include_router(assembly.router, tags=["assembly"], prefix="/assembly")
 api_router.include_router(channels.router, tags=[], prefix="/channels")
 api_router.include_router(content.router, tags=[], prefix="/content")
