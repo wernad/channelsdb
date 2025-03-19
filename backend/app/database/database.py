f4c8baf66f27903b8dca3e67c1e09227b1b969cb
chiro <jztnem@gmail.com>
2025-03-19 13:32:40 +0100

    add enviroment variables if available

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,15 +1,16 @@
 from sqlmodel import create_engine, SQLModel
 from app.config import DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 from pydantic_core import MultiHostUrl
+from os import environ
 
 DATABASE_URL = str(
     MultiHostUrl.build(
         scheme="postgresql",
-        username=DB_USER,
-        password=DB_PASSWORD,
-        host=DB_HOST,
-        port=DB_PORT,
-        path=DB_NAME,
+        username=environ.get("POSTGRES_USER", DB_USER),
+        password=environ.get("POSTGRES_PASSWORD", DB_PASSWORD),
+        host=environ.get("POSTGRES_HOST", DB_HOST),
+        port=int(port) if (port := environ.get("POSTGRES_PORT", None)) else DB_PORT,
+        path=environ.get("POSTGRES_NAME", DB_NAME),
     )
 )
 
