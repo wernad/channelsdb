0e97fdf84ad29a369080ec59c81dff22d2bb24ee
chiro <jztnem@gmail.com>
2025-03-19 13:32:20 +0100

    update password for local testing

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,8 +1,9 @@
-DB_HOST = "172.17.0.1"
+# DB_HOST = "172.17.0.1"
+DB_HOST = "localhost"
 DB_PORT = 5432
 DB_NAME = "channelsdb"
 DB_USER = "channelsdb"
-DB_PASSWORD = "local"
+DB_PASSWORD = "admin"
 
 
 API_PATH = "/api/v1"
