23fbbb2a0e7b87da5c12d75fef65cb481db0e450
chiro <jztnem@gmail.com>
2025-03-19 21:45:14 +0100

    update imports

--- backend/app/database/models/__init__.py
+++ backend/app/database/models/__init__.py
@@ -1,8 +1,18 @@
-from app.database.models.annotation import Annotation, AnnotationOutput
+from app.database.models.annotation import (
+    Annotation,
+    AnnotationOutput,
+    AnnotationsOutput,
+)
 from app.database.models.category import Category
 from app.database.models.channel_config import ConfigFile
 from app.database.models.het_residue import HetResidue
-from app.database.models.layer import Layer, Layers, LayerInfo, LayerGeometry, LayerProperties
+from app.database.models.layer import (
+    Layer,
+    Layers,
+    LayerInfo,
+    LayerGeometry,
+    LayerProperties,
+)
 from app.database.models.layer_residue import LayerResidue
 from app.database.models.method import Method
 from app.database.models.pdb import PDBData
