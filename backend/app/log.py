1a9364543e4e574840316da708f1f93dac61e9e7
chiro <jztnem@gmail.com>
2025-03-19 13:33:25 +0100

    add logger object

---
+++ backend/app/log.py
@@ -0,0 +1,10 @@
+import logging
+
+__all__ = ["logger"]
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+logger = logging.getLogger(__name__)
