0318a61257108881038d844885725416f233c108
chiro <jztnem@gmail.com>
2025-03-19 15:21:40 +0100

    remove unused imports

--- backend/app/api/endpoints/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.api.endpoints.ping import ping
-from app.api.endpoints.annotations import get_annotations_pdb
-from app.api.endpoints.channels import get_channels
-from app.api.endpoints.content import get_content
-from app.api.endpoints.statistics import get_statistics
-from app.api.endpoints.download import download_pdb
