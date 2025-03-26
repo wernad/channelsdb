718f292879b87ef39a0b72bbb8b96c035daaec6c
chiro <jztnem@gmail.com>
2025-03-26 21:05:00 +0100

    update statistics endpoint

--- backend/app/api/endpoints/statistics.py
+++ backend/app/api/endpoints/statistics.py
@@ -1,29 +1,33 @@
-import json
-from pathlib import Path
+from datetime import date
 
 from fastapi import APIRouter
-from pydantic import BaseModel, create_model
+from sqlmodel import SQLModel
 
-# from app.api.config import config
-from app.api.common import CHANNEL_TYPES
+from app.api.dependencies import StatisticsServiceDep
+
 
 router = APIRouter()
 
-TunnelModel = create_model("TunnelTypes", **{tunnel: (int, ...) for tunnel in CHANNEL_TYPES.values()})
 
-
-class StatisticsModel(BaseModel):
-    date: str
+class StatisticsModel(SQLModel):
+    date: date
     entries_count: int
-    statistics: TunnelModel
+    statistics: dict[str, int]
 
 
 @router.get(
     "/statistics",
     name="General statistics",
     description="Returns summary statistics about the data stored",
+    response_model=StatisticsModel,
 )
-async def get_statistics() -> StatisticsModel:
-    # with open(Path(config["dirs"]["base"]) / "statistics.json") as f:
-    #     return json.load(f)
-    return "No statistics found"
+async def get_channel_counts_by_software_method(
+    statistics_service: StatisticsServiceDep,
+):
+    statistics = statistics_service.get_channel_counts_by_software_method()
+
+    if statistics:
+        result = StatisticsModel(**statistics)
+        return result
+
+    return {}
