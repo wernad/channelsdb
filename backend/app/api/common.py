8bb356fbe20883ae9c66610e24b70bd0eff844f3
chiro <jztnem@gmail.com>
2025-03-26 21:04:19 +0100

    remove unused constants

--- backend/app/api/common.py
+++ backend/app/api/common.py
@@ -1,65 +1,6 @@
-from fastapi import Path as AnnotationPath, HTTPException
-from typing import Annotated
-from pydantic import AfterValidator
-from enum import Enum
-import requests
-from pydantic import BaseModel
-
-
-class IDError(BaseModel):
-    detail: str
-
-
-def validate_uniprot_id(uniprot_id: str):
-    req = requests.head(f"https://rest.uniprot.org/uniprotkb/{uniprot_id}.txt")
-    if req.status_code > 500:
-        raise HTTPException(
-            status_code=503,
-            detail=f"UniProt API returned an error when accessing: {req.url}",
-        )
-    if req.status_code != 200:
-        raise HTTPException(
-            status_code=404, detail=f"Cannot find Uniprot ID '{uniprot_id}'"
-        )
-    return uniprot_id
-
-
-def validate_pdb_id(pdb_id: str):
-    req = requests.head(f"https://www.ebi.ac.uk/pdbe/api/pdb/entry/summary/{pdb_id}")
-    if req.status_code > 500:
-        raise HTTPException(
-            status_code=503,
-            detail=f"PDBe API returned an error when accessing: {req.url}",
-        )
-    if req.status_code != 200:
-        raise HTTPException(status_code=404, detail=f"Cannot find PDB ID '{pdb_id}'")
-    return pdb_id
-
-
-pdb_id_404_response = {404: {"description": "PDB ID not found", "model": IDError}}
-uniprot_id_404_response = {
-    404: {"description": "Uniprot ID not found", "model": IDError}
-}
-
-
-PDB_ID_REGEX = "^[1-9][a-z0-9]{3}$"
+OLD_PDB_ID_REGEX = "^[1-9][a-z0-9]{3}$"
+NEW_PDB_ID_REGEX = "^pdb_[0-9]{5}[a-z0-9]{3}$"
 UNIPROT_ID_REGEX = "^[a-zA-Z0-9]+$"
-
-PDB_ID_Type = Annotated[
-    str,
-    AnnotationPath(description="PDB ID", pattern=PDB_ID_REGEX),
-    AfterValidator(validate_pdb_id),
-]
-Uniprot_ID_Type = Annotated[
-    str,
-    AnnotationPath(description="Uniprot ID", pattern=UNIPROT_ID_REGEX),
-    AfterValidator(validate_uniprot_id),
-]
-
-
-class SourceDatabase(Enum):
-    PDB = "PDB"
-    AlphaFill = "AlphaFill"
 
 
 CHANNEL_TYPES_PDB = {
