a18ec8babf8adccb45f8060efb47b7bfef799fd9
chiro <jztnem@gmail.com>
2025-03-26 21:04:44 +0100

    update id check, add statistics service dependency

--- backend/app/api/dependencies.py
+++ backend/app/api/dependencies.py
@@ -1,12 +1,14 @@
 from collections.abc import Generator
 from typing import Annotated
 
-from fastapi import Depends
+from fastapi import Depends, Path
 from sqlmodel import Session
+
+from app.api.common import OLD_PDB_ID_REGEX, NEW_PDB_ID_REGEX, UNIPROT_ID_REGEX
 from app.database.database import engine
 from app.database.repositories import ChannelRepository
 from app.services import AnnotationService, ChannelService, ExportService
-from app.api.exceptions import UnsupportedIDFormat
+from app.services.statistics import StatisticsService
 
 __all__ = [
     "AnnotationServiceDep",
@@ -59,20 +61,17 @@ def get_export_service(db: SessionDep) -> Generator[ExportService, None, None]:
 
 ExportServiceDep = Annotated[ExportService, Depends(get_export_service)]
 
+# Statistics
+
+
+def get_statistics_service(db: SessionDep) -> Generator[StatisticsService, None, None]:
+    yield StatisticsService(db)
+
+
+StatisticsServiceDep = Annotated[StatisticsService, Depends(get_statistics_service)]
+
 # ID handling
 
-
-def check_protein_id(protein_id: str):
-    if len(protein_id) == 4:  # Old PDB ID
-        protein_id = f"pdb_0000{protein_id}"
-    elif len(protein_id) == 12 and protein_id.startswith("pdb_"):  # New PDB id
-        pass
-    elif len(protein_id) == 6 and protein_id.startswith("P"):  # UniProt ID
-        pass
-    else:
-        raise UnsupportedIDFormat(protein_id=protein_id)
-
-    return protein_id
-
-
-IDCheckDep = Annotated[str, Depends(check_protein_id)]
+IDCheckDep = Annotated[
+    str, Path(pattern=f"{OLD_PDB_ID_REGEX}|{NEW_PDB_ID_REGEX}|{UNIPROT_ID_REGEX}")
+]
