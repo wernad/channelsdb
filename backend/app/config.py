4bcc69bf862dd673b4fc1b1f590cab06b356e271
chiro <jztnem@gmail.com>
2025-04-01 05:18:08 +0200

    add URL for image fetching

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,3 +7,11 @@ DB_PASSWORD = "admin"
 
 
 API_PATH = "/api/v1"
+
+PDB_HTTP_FILE_URL = (
+    "https://files-versioned.wwpdb.org/pdb_versioned/views/all/coordinates/mmcif/"
+)
+PDB_HTTP_ASSEMBLY_URL = "https://www.ebi.ac.uk/pdbe/api/pdb/entry/summary/"
+PDB_HTTP_IMAGE_URL = "https://www.ebi.ac.uk/pdbe/static/entry/|protein_id|_assembly_|assembly_id|_chemically_distinct_molecules_front_image-200x200.png"
+
+ALPHAFILL_HTTP_FILE_URL = "https://alphafill.eu/v1/aff/"
