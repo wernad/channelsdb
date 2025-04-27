e484260ff611d0e321a4d5b92459a43082ea8fba
chiro <jztnem@gmail.com>
2025-04-27 22:05:39 +0200

    add load/calculation script for pdb proteins

---
+++ backend/app/tunnels/fetch/common.py
@@ -0,0 +1,197 @@
+import json
+from urllib.parse import quote_plus
+
+from requests import Response, get
+from requests.exceptions import ConnectTimeout
+
+from app.config import PDB_DATA_API_URL, PDB_HTTP_FILE_URL, PDB_SEARCH_API_URL
+from app.log import log
+
+
+def get_full_id(id: str):
+    """Returns 12-character id of given 4-character id."""
+    return f"pdb_0000{id.lower()}"
+
+
+def get_error_message(response: Response) -> str:
+    """Extracts error message if any given, otherwise returns plain text."""
+    log.debug("Extractng error message.")
+    try:
+        message = response.json()
+    except Exception as _:
+        message = response.text
+
+    return message
+
+
+def get_file_url(id: str, version: str) -> str:
+    """Create file url based on id and version.
+
+    Parameters:
+        id: structure id
+        version: version to fetch
+    Returns:
+        tuple of bytes and error code
+    """
+    log.debug(f"Creating url for file - {id=} {version=}.")
+    id = id.lower()
+    full_id = get_full_id(id)
+    category = id[1:3]
+    file_name = f"{full_id}_xyz_v{version}.cif.gz"
+    url = f"{PDB_HTTP_FILE_URL}{category}/{full_id}/{file_name}"
+    log.debug(f"Created file url: {url}")
+    return url
+
+
+def get_graphql_query(id: str) -> str:
+    """Helper method to create url encoded string for Data API."""
+    log.debug(f"Generating GraphQL query string for id {id}")
+
+    query = (
+        f'{{entry(entry_id: "{id}"){{pdbx_audit_revision_history{{major_revision}}}}}}'
+    )
+    encoded = quote_plus(query)
+
+    url = f"{PDB_DATA_API_URL}?query={encoded}"
+    return url
+
+
+def get_search_url(start: int = 0, limit: int = 1000) -> str:
+    """Helper method to create a url encoded string for Search API."""
+    log.debug(f"Creating search query string with these params: {start=}, {limit=}")
+    params = {
+        "query": {
+            "type": "terminal",
+            "service": "text",
+            "parameters": {
+                "attribute": "rcsb_accession_info.initial_release_date",
+                "operator": "exists",
+            },
+        },
+        "request_options": {"paginate": {"start": start, "rows": limit}},
+        "return_type": "entry",
+    }
+
+    json_params = json.dumps(params)
+    encoded = quote_plus(json_params)
+    url = f"{PDB_SEARCH_API_URL}?json={encoded}"
+    log.debug(f"URL string created. {url}")
+    return url
+
+
+def get_last_version(id: str) -> int | None:
+    """Fetches latest version number of given file ID."""
+    log.debug(f"Fetching latest version of a file with ID {id}.")
+
+    url = get_graphql_query(id)
+    log.debug(f"Query for {id}: {url}")
+
+    response = get(url)
+    if response.status_code == 200:
+        body = response.json()
+        version = body["data"]["entry"]["pdbx_audit_revision_history"][-1][
+            "major_revision"
+        ]
+        log.debug(f"File {id} - latest version: {version}.")
+        return version
+
+    message = get_error_message(response)
+    log.debug(f"No version retrieved for id {id} - error: {message}")
+    return None
+
+
+def get_all_versions(id: str) -> set[int]:
+    """Fetches all versions of given structure, if any.
+
+    Args:
+        id: id of entry.
+
+    Returns:
+        version in a set of integers.
+    """
+
+    log.debug(f"Fetching all versions of file with ID {id}.")
+
+    query = get_graphql_query(id)
+
+    url = f"{PDB_DATA_API_URL}?query={query}"
+    response = get(url)
+
+    if response.status_code == 200:
+        body = response.json()
+        data = body["data"]["entry"]["pdbx_audit_revision_history"]
+
+        versions = set([x["major_revision"] for x in data])
+
+        log.debug(f"File {id} - versions found: {versions}.")
+        return versions
+
+    message = get_error_message(response)
+    log.debug(f"No versions retrieved for id {id} - error: {message}")
+    return None
+
+
+def fetch_files(urls: list[str]) -> list[bytes]:
+    """Fetches files from given urls.
+
+    Parameters:
+        urls: list of urls
+    Returns:
+        list of bytes
+    """
+    log.debug(f"Fetching {len(urls)} files.")
+    files = []
+
+    for url in urls:
+        response = get(url)
+
+        if response.status_code == 200:
+            files.append(response.content)
+    log.debug("Fetching done.")
+    return files
+
+
+def get_file(url: list[str]) -> list[bytes]:
+    """Fetches files from given urls.
+
+    Parameters:
+        urls: list of urls
+    Returns:
+        list of bytes
+    """
+    log.debug(f"Fetching file from url: {url}")
+    finished = False
+    while not finished:
+        try:
+            response = get(url)
+            code = response.status_code
+            if code == 200:
+                log.debug("Fetching complete.")
+                finished = True
+                return response.content
+            log.error(f"Unexpected status code {code} for url: {url}")
+        except ConnectTimeout as _:
+            log.error(f"Connection timed out on url: {url}")
+
+    log.error(f"Fetching failed for url: {url}")
+
+
+def fetch_file_at_version(id: str, version: str) -> tuple:
+    """Fetches file with given id at given version.
+
+    Parameters:
+        id: 12-character structure id
+        version: version to fetch
+    Returns:
+        tuple of bytes and error code
+    """
+
+    category = id[1:3].lower()
+    file_name = f"{id}_xyz_v{version}.cif.gz"
+    url = f"{PDB_HTTP_FILE_URL}{category}/{id}/{file_name}"
+    response = get(url)
+
+    if response.status_code == 200:
+        body = response.content
+        return body, None
+    return None, response.status_code
---
+++ backend/app/tunnels/fetch/pdb.py
@@ -0,0 +1,223 @@
+import concurrent.futures as cf
+import logging
+import multiprocessing as mp
+import os
+import shutil
+from gzip import GzipFile
+from io import BytesIO
+from math import ceil
+from queue import Full
+from time import sleep
+
+from requests import get
+
+from app.config import (OUTPUT_PATH, PDB_SEARCH_API_LIMIT, QUEUE_SIZE,
+                        WORKER_LIMIT)
+from app.log import log
+from app.tunnels.fetch.common import (get_file, get_file_url, get_full_id,
+                                      get_last_version, get_search_url)
+from app.tunnels.fetch.workers import create_inserter, create_managers
+
+
+def fetch_ids(start: int, limit: int) -> list[str]:
+    """Fetches a list of ids based on start and limit.
+
+    Parameters:
+        start: sequence start
+        limit: number of ids
+    Return:
+        list[str]
+    """
+
+    url = get_search_url(start=start, limit=limit)
+
+    response = get(url)
+
+    if response.status_code == 200:
+        return response.json()
+
+
+def get_latest_versions(ids: list[str]) -> dict:
+    """Returns dictionary of latest versions of each protein ID."""
+
+    with cf.ThreadPoolExecutor(max_workers=WORKER_LIMIT) as executor:
+        id_to_version = dict(zip(ids, executor.map(get_last_version, ids)))
+
+    return id_to_version
+
+
+def get_file_urls(ids: list[str], id_to_version: dict) -> dict[str, str]:
+    """Returns url for fetching latest file for each protein ID."""
+
+    file_urls = {}
+    for id in ids:
+        version = id_to_version[id]
+        file_urls[id] = get_file_url(id, version)
+
+    return file_urls
+
+
+def fetch_files(file_urls: dict, id_to_version: dict) -> tuple[list, list]:
+    """Fetches files from given urls and returns SQLModel objects for insertion, also returns ids that failed."""
+    with cf.ThreadPoolExecutor(max_workers=WORKER_LIMIT) as executor:
+        id_to_data = dict(
+            zip(file_urls.keys(), executor.map(get_file, file_urls.values()))
+        )
+
+    failed = []
+    files = []
+
+    for id, data in id_to_data.items():
+        if data:
+            full_id = get_full_id(id)
+            version = id_to_version[id]
+            data_io = BytesIO(data)
+            with GzipFile(fileobj=data_io, mode="rb") as gz_file:
+                cif_file = gz_file.read()
+            files.append((full_id, version, cif_file))
+        else:
+            failed.append(id)
+
+    return files, failed
+
+
+# TODO check workers because it gets stuck after queue is full with no logs from workers -> no processing ?
+def fetch_all(start: int, total: int, data_queue: mp.Queue) -> None:
+    """Fetches IDs and corresponding latest file entries and stores them in database.
+
+    Pushes files to data_queue for processing and waits if queue is full.
+    Uses explicit timeouut to avoid overwhelming PDB APIs.
+
+    Args:
+        start: starting id.
+        total: total number of ids to work with.
+        data_queue: Queue for data processing.
+    """
+    log.debug("Entry fetching started.")
+
+    logging.getLogger("requests").setLevel(logging.WARNING)
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+    starts = [x for x in range(start, total, PDB_SEARCH_API_LIMIT)]
+    log.debug(f"Created range starts: {starts}")
+
+    total_processed = 0
+    total_failed = 0
+
+    for start in starts:
+        url = get_search_url(start=start, limit=PDB_SEARCH_API_LIMIT)
+        response = get(url)
+        if response.status_code == 200 and "result_set" in (
+            formatted := response.json()
+        ):
+            ids = [entry["identifier"] for entry in formatted["result_set"]]
+            log.debug(f"Received {len(ids)} ids.")
+
+            id_to_version = get_latest_versions(ids)
+            file_urls = get_file_urls(ids, id_to_version)
+
+            files_to_process, failed_batch = fetch_files(file_urls, id_to_version)
+
+            while len(files_to_process) > 0:
+                try:
+                    file = files_to_process.pop(0)
+                    data_queue.put(file)
+                except Full:
+                    files_to_process.insert(0, file)
+                    log.debug("Data queue is full, waiting...")
+                    sleep(2)
+
+            total_processed += len(ids)
+            total_failed += len(failed_batch)
+            if failed_batch:
+                log.debug(f"Number of failed ids: {len(failed_batch)}")
+                with open("failed.txt", "a+") as file:
+                    file.write("\n".join([x for x in failed_batch]))
+        log.debug(
+            f"Total processed: {total_processed} -- Total failed: {total_failed}."
+        )
+
+        # DO NOT DELETE!!!
+        sleep(5)  # Required to avoid 'Too many requests' error
+        # DO NOT DELETE!!!
+
+    log.debug("Entry fetching finished.")
+
+    logging.getLogger("requests").setLevel(logging.DEBUG)
+    logging.getLogger("urllib3").setLevel(logging.DEBUG)
+
+
+def get_linspace(total: int):
+    "Helper method to calculate step and starts for each worker."
+
+    step = int(ceil(total / WORKER_LIMIT))
+    starts = (0 + i * step for i in range(WORKER_LIMIT))
+
+    return step, starts
+
+
+def _create_output_directory() -> None:
+    """Creates output folder for workers."""
+    log.debug("Creating output folder structure.")
+
+    try:
+        os.mkdir(OUTPUT_PATH)
+        log.debug("Directory created successfully.")
+    except FileExistsError:
+        shutil.rmtree(OUTPUT_PATH)
+        log.debug(f"Directory '{OUTPUT_PATH}' already exists, cleaning up.")
+        os.mkdir(OUTPUT_PATH)
+        log.debug("Directory re-created successfully.")
+    except PermissionError:
+        log.error(f"Permission denied: Unable to create '{OUTPUT_PATH}'.")
+    except Exception as e:
+        log.error(f"An error occurred: {e}")
+
+
+# TODO handle remote and mirror fetching
+def run(start: int | None, pdb: str):
+    """Creates and starts child processes for fetching file data.
+
+    Args:
+        start: starting id for fetching.
+    """
+    log.info("Beggining fetch of all PDB entries.")
+    url = get_search_url(start=0, limit=0)
+
+    response = get(url)
+
+    _create_output_directory()
+
+    if response.status_code == 200:
+        total = response.json()["total_count"]
+        log.debug(f"Total number of entries: {total}")
+        actual_start = start if start else 0
+
+        log.debug("Creating managers...")
+        data_queue = mp.Queue(maxsize=QUEUE_SIZE)
+        result_queue = mp.Queue()
+
+        managers = create_managers(data_queue=data_queue, result_queue=result_queue)
+        inserter = create_inserter(result_queue=result_queue)
+
+        log.debug("Workers created, starting main function...")
+        fetch_all(start=actual_start, total=total, data_queue=data_queue)
+
+        log.debug(
+            "Tunnels calculations complete. Passing shutdown message to queues..."
+        )
+        data_queue.put(None)
+        result_queue.put(None)
+
+        log.debug("Waiting for workers to stop.")
+        for manager in managers:
+            manager.join()
+
+        inserter.join()
+        log.info("Script finished.")
+    else:
+        log.error(f"Received unexpected status code: {response.status_code}")
+
+
+if __name__ == "__main__":
+    run()
