ebd8fe3533de91238e69d3651c33c04e36f6b63c
chiro <jztnem@gmail.com>
2025-05-05 08:20:06 +0200

    add extra methods

--- backend/app/services/structure.py
+++ backend/app/services/structure.py
@@ -1,6 +1,6 @@
 from sqlmodel import Session
 
-from app.database.models import StructureData, StructureInsert
+from app.database.models import StructureData, StructureInsert, Structure
 from app.database.repositories import StructureRepository
 
 
@@ -10,12 +10,27 @@ class StructureService:
     def __init__(self, db: Session):
         self.repository = StructureRepository(db)
 
-    def check_if_exists_by_external_id(self, external_id: str) -> int | None:
+    def check_if_exists_by_external_id(self, external_id: str) -> Structure | None:
         """Checks if given structure exists using external id."""
         result = self.repository.get_structure_by_external_id(external_id)
 
         if result:
-            return result.id
+            return result
+
+        return None
+
+    def update_has_channels_by_internal_id(
+        self, internal_id: int, has_channels: bool
+    ) -> None:
+        """Updates has_channels column of given protein entry."""
+
+        result = self.repository.update_has_channels_by_internal_id(
+            internal_id, has_channels
+        )
+
+        if result:
+            return result
+
         return None
 
     def get_source_and_version_by_id(self, external_id: str) -> StructureData:
@@ -31,10 +46,22 @@ class StructureService:
         if structure:
             return structure.id
 
+        return None
+
     def insert_entry(self, values: StructureInsert) -> int:
         """Inserts a single row into layer table."""
 
         result = self.repository.insert_entry(values=values)
+
+        if result:
+            return result
+
+        return None
+
+    def get_external_from_internal_bulk(self, internal_ids: list[int]) -> list[str]:
+        """Returns list of external ids from corresponding internal ids."""
+
+        result = self.repository.get_external_from_internal_bulk(internal_ids)
 
         if result:
             return result
