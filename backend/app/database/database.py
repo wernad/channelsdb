d7d4c4d897aeac872e25c7447b8126c68c10b559
chiro <jztnem@gmail.com>
2025-05-05 08:22:24 +0200

    update imports

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -7,8 +7,12 @@ from pydantic_core import MultiHostUrl
 from sqlmodel import Session, SQLModel, create_engine, inspect, text
 
 from app.config import DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER
-from app.database.repositories import (CategoryRepository, MethodRepository,
-                                       ResidueRepository, SourceRepository)
+from app.database.repositories import (
+    CategoryRepository,
+    MethodRepository,
+    ResidueRepository,
+    SourceRepository,
+)
 from app.log import log
 
 __all__ = ["db_context", "create_db_and_tables", "get_session"]
