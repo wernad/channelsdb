7d93db45e20a65275289fba04eeb577cd8ca6c7c
chiro <jztnem@gmail.com>
2025-05-16 17:37:51 +0200

    update return value format in statistics selects

--- backend/app/services/statistics.py
+++ backend/app/services/statistics.py
@@ -27,7 +27,11 @@ class StatisticsService:
         date = dt.now().date()
         if result:
             entries_count = sum(row.count for row in result)
-            entries = {f"{row.method}": row.count for row in result}
+
+            entries = {method: 0 for method in METHODS_IDS_TO_NAMES.values()}
+            for row in result:
+                entries[row.method] = row.count
+
             result = {
                 "date": date,
                 "entries_count": entries_count,
@@ -42,8 +46,8 @@ class StatisticsService:
 
         return result
 
-    def get_channel_counts_by_id(self, structure_id: int) -> dict:
-        result = self.channel_repository.get_channel_counts_by_id(
+    def get_channel_counts_per_methods_by_id(self, structure_id: int) -> dict:
+        result = self.channel_repository.get_channel_counts_per_methods_by_id(
             structure_id=structure_id
         )
 
@@ -51,6 +55,7 @@ class StatisticsService:
         date = dt.now().date()
         if result:
             entries_count = sum(row.count for row in result)
+
             entries = {method: 0 for method in METHODS_IDS_TO_NAMES.values()}
             for row in result:
                 entries[row.method] = row.count
@@ -73,17 +78,33 @@ class StatisticsService:
         """Returns top 5 external protein ids with most channels."""
         result = self.channel_repository.get_top_5_channel_counts_per_protein()
 
+        if not result:
+            return None
+
+        total = self.channel_repository.get_total_channels()
+
+        result["total"] = total
+
         return result
 
-    def get_top_5_residues_by_channel(self) -> list[str]:
+    def get_top_5_residues_by_channel_count(self) -> list[str]:
         "Returns top 5 most common residues in channels."
         result = self.residue_repository.get_top_residue_counts_by_channels()
 
+        if not result:
+            return None
+
+        total = self.channel_repository.get_total_channels()
+        result["total"] = total
+
         return result
 
-    def get_top_5_categories_as_ratios(self) -> dict:
-        """Returns top 5 categories of channels as a ratio"""
-        result = self.channel_repository.get_top_channel_counts_per_category_ratio()
+    def get_top_5_types_by_channel_count(self) -> dict:
+        """Returns top 5 types of channels."""
+        result = self.channel_repository.get_top_n_channel_counts_per_type()
+
+        if not result:
+            return None
 
         return result
 
