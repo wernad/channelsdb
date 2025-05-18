6229930cca4d50f5f5cf50d345c538e1cbd1e621
chiro <jztnem@gmail.com>
2025-05-18 20:11:47 +0200

    add CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,13 @@ from contextlib import asynccontextmanager
 
 from fastapi import APIRouter, FastAPI
 from psycopg2 import OperationalError
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api.main import api_router
 from app.config import API_PATH
 from app.database.database import create_db_and_tables, init_flag_data
 from app.log import log
+from app.channels.data.scheduler import get_scheduler
 
 router = APIRouter()
 router.include_router(api_router, prefix=API_PATH)
@@ -19,8 +21,10 @@ async def lifespan(app: FastAPI):
         init_flag_data()
     except OperationalError as e:
         log.error(f"An operational error occured white creating tables: {e.pgcode}")
-
+    # scheduler = get_scheduler()
+    # scheduler.start()
     yield
+    # scheduler.shutdown()
 
 
 app = FastAPI(
@@ -31,6 +35,14 @@ app = FastAPI(
     version="beta",
     swagger_ui_parameters={"syntaxHighlight": False, "defaultModelsExpandDepth": -1},
     lifespan=lifespan,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
