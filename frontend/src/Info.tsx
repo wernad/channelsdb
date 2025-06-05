293cba5068c077791441a74df479e30c2445ad77
chiro <jztnem@gmail.com>
2025-06-05 13:31:57 +0200

    update URLs in Info component

--- frontend/src/Info.tsx
+++ frontend/src/Info.tsx
@@ -44,7 +44,7 @@ export class Intro extends React.Component<{ state: State }, { statistics: any, 
                         &nbsp;<small>contains:</small><b> {stats ? stats.entries_count : 'n/a'}</b> protein entries
                         &nbsp;{this.state.statisticsExpanded ? <a href='_blank' onClick={(e) => this.setStatistics(e, false)}>Hide details</a> : <a href='_blank' onClick={(e) => this.setStatistics(e, true)}>Show details</a>}
                     </div>
-                    {this.state.statisticsExpanded ? <Statistics statistics={this.state.statistics}/> : ''}
+                    {this.state.statisticsExpanded ? <Statistics statistics={this.state.statistics} /> : ''}
                 </div>
                 <div className='col-lg-9' style={{ textAlign: 'left', textJustify: 'inter-word', padding: '0' }}>
 
@@ -52,8 +52,8 @@ export class Intro extends React.Component<{ state: State }, { statistics: any, 
 
                     <p className='text-justify'>
                         The database contains information about channel positions, geometry and physicochemical properties. Additionally, all the entries are crosslinked with
-                            the <a href='https://www.uniprot.org' target='_blank'>UniProt database</a> a comprehensive high-quality resource of protein function information. Last but not least, all the results are displayed in
-                            a clear interactive manner further facilitating data interpretation. </p>
+                        the <a href='https://www.uniprot.org' target='_blank'>UniProt database</a> a comprehensive high-quality resource of protein function information. Last but not least, all the results are displayed in
+                        a clear interactive manner further facilitating data interpretation. </p>
 
                     <p>If you would like to provide your own research results to be displayed soon as a part of Protein Data Bank in Europe. <a href='mailto:webchemistryhelp@gmail.com'>Get in touch with us</a>, or use the <a href='contribute.html'>annotation form</a>.</p>
 
@@ -66,7 +66,7 @@ export class Intro extends React.Component<{ state: State }, { statistics: any, 
                         </small>
                     </p>
                 </div>
-                <div className='channelsdb-logo col-lg-3'><a href='https://dx.doi.org/10.1093/nar/gkx868' target='_blank'><img src={'assets/img/channelsdb_cover.png'} style={{width: '100%'}} /></a></div>
+                <div className='channelsdb-logo col-lg-3'><a href='https://dx.doi.org/10.1093/nar/gkx868' target='_blank'><img src={'assets/img/channelsdb_cover.png'} style={{ width: '100%' }} /></a></div>
             </div>
         </div>;
     }
@@ -100,21 +100,21 @@ export class Info extends React.Component<{ state: State }, {}> {
                     <div className='well'>
                         <div>
                             <div className='col-lg-4'>
-                                <a href='/detail/pdb/1ymg'>
+                                <a href='/detail?pid=1ymg'>
                                     <img style={centerStyle} className='img-circle' src={'assets/img/1ymg.png'} alt='1ymg channel detail' width='140' height='140' />
                                 </a>
                                 <a href='#ex-1ymg' role='button'><h3>Aquaporin water channel</h3></a>
                                 <p style={justify}>The pore architecture of Aquaporin O at 2.2&#8491; resolution highlights residues critical for water permeation regulation.</p>
                             </div>
                             <div className='col-lg-4'>
-                                <a href='/detail/pdb/4nm9'>
+                                <a href='/detail?pid=4nm9'>
                                     <img style={centerStyle} className='img-circle' src={'assets/img/4nm9.png'} alt='4nm9 channel detail' width='140' height='140' />
                                 </a>
                                 <a href='#ex-4nm9' role='button'><h3>Substrate channeling system</h3></a>
                                 <p style={justify}>Proline utilization A protein contains two active sites separated by ~75&#8491; long channeling system accompanied by a complex network of channels.</p>
                             </div>
                             <div className='col-lg-4'>
-                                <a href='/detail/pdb/1jj2'>
+                                <a href='/detail?pid=1jj2'>
                                     <img style={centerStyle} className='img-circle' src={'assets/img/1jj2.png'} alt='1jj2 channel detail' width='140' height='140' />
                                 </a>
                                 <a href='#ex-1jj2' role='button'><h3>Ribosomal polypeptide exit tunnel</h3></a>
@@ -128,21 +128,21 @@ export class Info extends React.Component<{ state: State }, {}> {
                         </div>
                         <div>
                             <div className='col-lg-4'>
-                                <a href='/detail/pdb/3tbg'>
+                                <a href='/detail?pid=3tbg'>
                                     <img style={centerStyle} className='img-circle' src={'assets/img/3tbg.png'} alt='3tbg channel detail' width='140' height='140' />
                                 </a>
                                 <a href='#ex-3tbg' role='button'><h3>Cytochrome P450 2D6 tunnels in experimental structure</h3></a>
                                 <p style={justify}>Cytochromes P450 are known for complex net of multiple channels leading towards the active site. These channels serve multiple roles.</p>
                             </div>
                             <div className='col-lg-4'>
-                                <a href='/detail/alphafill/P10635'>
+                                <a href='/detail?pid=P10635'>
                                     <img style={centerStyle} className='img-circle' src={'assets/img/alphafill_example.png'} alt='P10635 channel detail' width='140' height='140' />
                                 </a>
                                 <a href='#ex-P10635' role='button'><h3>Cytochrome P450 2D6 tunnels in AlphaFill model</h3></a>
                                 <p style={justify}>Complex net of multiple channels in cytochrome P450 2D6 leading towards the active site is visible even in AlphaFold structures.</p>
                             </div>
                             <div className='col-lg-4'>
-                                <a href='/detail/pdb/5lka'>
+                                <a href='/detail?pid=5lka'>
                                     <img style={centerStyle} className='img-circle' src={'assets/img/examples-figure-HLD.png'} alt='HLD channel detail' width='140' height='140' />
                                 </a>
                                 <a href='#ex-HLD' role='button'><h3>Haloalkane dehalogenase LinB with engineered tunnels</h3></a>
@@ -161,7 +161,7 @@ export class Info extends React.Component<{ state: State }, {}> {
             <div className='row featurette' style={{ marginTop: '40px' }}>
                 {React.createElement('a' as any, { 'name': 'ex-1ymg' })}
                 <div className='col-md-7'>
-                    <a href='/detail/pdb/1ymg'><h2 className='featurette-heading'>Aquaporin O <span className='text-muted'>(1ymg)</span></h2></a>
+                    <a href='/detail?pid=1ymg'><h2 className='featurette-heading'>Aquaporin O <span className='text-muted'>(1ymg)</span></h2></a>
                     <p style={justify} className='lead'>The pore architecture of Aquaporin O at 2.2&#8491; resolution highlights residues critical for water permeation regulation.</p>
                     <p style={justify}>The channel is ~ 30&#8491; long and highlights with some of the residues crucial for its proper function. Selectivity filter (ar/R), which allows water molecules passage through the membrane in a single file (green sticks). Residues providing canonical AQP hydrogen bond acceptor that align waters through the channel in balls and stick model. Finally, Tyr-149 important for channel gating (orange).</p>
                     <p style={reference}><small><a target='_blank' href='https://dx.doi.org/10.1073/pnas.0405274101'>
@@ -177,7 +177,7 @@ export class Info extends React.Component<{ state: State }, {}> {
             <div className='row featurette'>
                 {React.createElement('a' as any, { 'name': 'ex-3tbg' })}
                 <div className='col-md-7 col-md-push-5'>
-                    <a href='/detail/pdb/3tbg'><h2 className='featurette-heading'>Cytochrome P450 2D6 experimental <br/>structures<span className='text-muted'>(3tbg)</span></h2></a>
+                    <a href='/detail?pid=3tbg'><h2 className='featurette-heading'>Cytochrome P450 2D6 experimental <br />structures<span className='text-muted'>(3tbg)</span></h2></a>
                     <p style={justify} className='lead'>Cytochromes P450 are known for complex net of multiple channels leading towards active site. These channels serve multiple roles in substrate access, product release or hydration pathways.</p>
                     <p style={justify}>Cytochrome  P450  2D6  contributes  significantly  to  the  metabolism  of  {'>'}15%  of  the  200  most marketed drugs. Cytochrome P450 2D6 structure shows a second molecule of thioridazine bound in an expanded substrate access channel (channel 2f according to <a href='https://doi.org/10.1016/j.bbagen.2006.07.005' target='_blank'>Cojocaru et al. classification</a>  antechamber  with  its  piperidine  moiety  forming  a charge-stabilized hydrogen bond with Glu-222.</p>
                     <p style={reference}><small><a target='_blank' href='https://dx.doi.org/10.1074/jbc.M114.627661'>
@@ -193,7 +193,7 @@ export class Info extends React.Component<{ state: State }, {}> {
             <div className='row featurette'>
                 {React.createElement('a' as any, { 'name': 'ex-P10635' })}
                 <div className='col-md-7 '>
-                    <a href='/detail/alphafill/P10635'><h2 className='featurette-heading'>Cytochrome P450 2D6 AlphaFill models<span className='text-muted'>(P10635)</span></h2></a>
+                    <a href='/detail?pid=P10635'><h2 className='featurette-heading'>Cytochrome P450 2D6 AlphaFill models<span className='text-muted'>(P10635)</span></h2></a>
                     <p style={justify} className='lead'>Cytochromes P450 channels starts at HEM cofactor, which can be used to guide channels even in AlphaFill models.</p>
                     <p style={justify}>As mentioned above, Cytochrome P450 2D6 contributes significantly to the metabolism of {'>'}15% of the 200 most marketed drugs. Cytochrome P450 2D6 structure shows a second molecule of thioridazine bound in an expanded substrate access channel (channel 2f according to Cojocaru et al. classification antechamber with its piperidine moiety forming a charge-stabilized hydrogen bond with Glu-222).</p>
                     <p style={reference}><small><a target='_blank' href='https://dx.doi.org/10.1074/jbc.M114.627661'>
@@ -209,7 +209,7 @@ export class Info extends React.Component<{ state: State }, {}> {
             <div className='row featurette'>
                 {React.createElement('a' as any, { 'name': 'ex-1jj2' })}
                 <div className='col-md-7 '>
-                    <a href='/detail/pdb/1jj2'><h2 className='featurette-heading'>Large Ribosomal Subunit <span className='text-muted'>(1jj2)</span></h2></a>
+                    <a href='/detail?pid=1jj2'><h2 className='featurette-heading'>Large Ribosomal Subunit <span className='text-muted'>(1jj2)</span></h2></a>
                     <p style={justify} className='lead'>The ribosomal polypeptide tunnel provides an insight into the release of a nascent polypeptide chain out of the ribosomal complex.</p>
                     <p style={justify}>The exit tunnel is surrounded by arginine side chains (stick model), bearing positive charges as well as RNA backbone phosphate groups (spheres), thus providing fragmental charge along the tunnel, which is necessary to prevent the nascent peptide from sticking to the channel wall inside the ribosome. Subunits L4, L22 and L39e interacting with the exit tunnel are highlighted in yellow, green and magenta respectivelly.</p>
                     <p style={reference}><small><a target='_blank' href='https://dx.doi.org/10.1016/j.jmb.2006.05.023'>
@@ -225,7 +225,7 @@ export class Info extends React.Component<{ state: State }, {}> {
             <div className='row featurette'>
                 {React.createElement('a' as any, { 'name': 'ex-4nm9' })}
                 <div className='col-md-7 col-md-push-5'>
-                    <a href='/detail/pdb/4nm9'><h2 className='featurette-heading'>PutA channeling system <span className='text-muted'>(4nm9)</span></h2></a>
+                    <a href='/detail?pid=4nm9'><h2 className='featurette-heading'>PutA channeling system <span className='text-muted'>(4nm9)</span></h2></a>
                     <p style={justify} className='lead'>Substrate channeling is a process of passing intermediate metabolic product from one reaction site to another through intramolecular tunnel.</p>
                     <p style={justify}>In Gram-negative bacteria a proline catabolism is exerted by a single protein combining two different enzymes commonly known as Proline utilization A protein (PutA).
                         The active sites of flavoenzyme proline dehydrogenase (PRODH) and ∆-1-pyrroline-5-carboxylate dehydrogenase (P5CDH) are connected by ~75&#8491; long channel throughout the hydrolysis
@@ -243,7 +243,7 @@ export class Info extends React.Component<{ state: State }, {}> {
             <div className='row featurette'>
                 {React.createElement('a' as any, { 'name': 'ex-HLD' })}
                 <div className='col-md-7 '>
-                    <h2 className='featurette-heading'><a href='/detail/pdb/5lka'>Haloalkane dehalogenase LinB engineered tunnels </a><span className='text-muted'>(<a href='/detail/pdb/1k63'>1k63</a>, <a href='/detail/pdb/4wdq'>4wdq</a>, <a href='/detail/pdb/5lka'>5lka</a>)</span></h2>
+                    <h2 className='featurette-heading'><a href='/detail?pid=5lka'>Haloalkane dehalogenase LinB engineered tunnels </a><span className='text-muted'>(<a href='/detail?pid=1k63'>1k63</a>, <a href='/detail?pid=4wdq'>4wdq</a>, <a href='/detail?pid=5lka'>5lka</a>)</span></h2>
                     <p style={justify} className='lead'>Application of computational design and directed evolution led to the design of a de novo transport tunnel in haloalkane dehalogenase LinB.</p>
                     <p style={justify}>Mutants with a blocked native main tunnel p1 and newly opened auxiliary tunnel p3 in a distinct part of the structure showed dramatically modified properties. Opening of the auxiliary
                         tunnel resulted in the changes of the specificity and substrate inhibition. Crystallographic analysis and molecular dynamics simulations confirmed the successful introduction of a structurally
@@ -260,13 +260,13 @@ export class Info extends React.Component<{ state: State }, {}> {
 
             <img className='img' src={'assets/img/elixirlogo.png'} alt='ELIXIR logo' height='70' />
             <img className='img' src={'assets/img/kfc.png'} alt='KFC logo' height='70' />
-            <img className='img' src={'assets/img/upol.png'} alt='UPOL logo' height='70'/>
-            <img className='img' src={'assets/img/recetox.png'} alt='RECETOX logo' height='70'/>
-            <img className='img' src={'assets/img/ceitec.png'} alt='CEITEC logo' height='70'/>
+            <img className='img' src={'assets/img/upol.png'} alt='UPOL logo' height='70' />
+            <img className='img' src={'assets/img/recetox.png'} alt='RECETOX logo' height='70' />
+            <img className='img' src={'assets/img/ceitec.png'} alt='CEITEC logo' height='70' />
             <div className='row well well-sm featurette text-center' style={{ marginTop: '10px' }}>
                 ChannelsDB is a part of services provided by <a href='https://www.elixir-czech.cz/' target='_blank'>ELIXIR</a> &ndash;
-                    European research infrastructure for biological information. This work was supported by ELIXIR CZ and RECETOX RI research infrastructure projects (MEYS) [LM2023055; LM2023069]
-                    including access to computing and storage facilities. For other services provided by ELIXIR's Czech Republic Node visit <a href='https://www.elixir-czech.cz/services' target='_blank'>www.elixir-czech.cz/services</a>.
+                European research infrastructure for biological information. This work was supported by ELIXIR CZ and RECETOX RI research infrastructure projects (MEYS) [LM2023055; LM2023069]
+                including access to computing and storage facilities. For other services provided by ELIXIR's Czech Republic Node visit <a href='https://www.elixir-czech.cz/services' target='_blank'>www.elixir-czech.cz/services</a>.
             </div>
 
             <ScrollButton scrollStepInPx='50' delayInMs='10' />
